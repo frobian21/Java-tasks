c967bb3e15667c09e3b861f611271c22b5765615
Fabian <frobian21@yahoo.co.uk>
2019-04-28 20:41:46 +0100

    added withdraw and deposit functions

--- BankingApp.java
+++ BankingApp.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.sql.*;
 import java.awt.event.*;
 import java.awt.*;
+//import static javax.swing.GroupLayout.Alignment.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class BankingApp {
 	JFrame win = new JFrame();
@@ -22,7 +25,9 @@ public class BankingApp {
 		win.setSize(400, 400);
 		win.setVisible(true);
 		m1.addActionListener(new OpenAccountList());
-
+		m2.addActionListener(new Transactions());
+		m3.addActionListener(new Transactions());
+		m4.addActionListener(new Transactions());
 	}
 
 	public static void main(String[] args) {
@@ -32,7 +37,6 @@ public class BankingApp {
 }
 
 class OpenAccountList implements ActionListener {
-
 	JTextField nameText = new JTextField(10);
 	JTextField addressText = new JTextField(10);
 	JRadioButton currentButton = new JRadioButton("Current");
@@ -57,14 +61,6 @@ class OpenAccountList implements ActionListener {
 		group1.add(savingsButton);
 		group2.add(maleButton);
 		group2.add(femaleButton);
-
-//		GroupLayout layout = new GroupLayout(top);
-//		top.setLayout(layout);
-//		layout.setAutoCreateGaps(true);
-//		layout.setAutoCreateContainerGaps(true);
-//		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
-//		hGroup.addGroup(layout.createParallelGroup().addComponent(nameLabel).addComponent(addressLabel));
-//		hGroup.addGroup(layout.createParallelGroup().addComponent(nameText).addComponent(addressText)); //cant get grouplayout to work
 
 		f.setLayout(new GridLayout(3, 1));
 		f.add(top);
@@ -134,7 +130,7 @@ class OpenAccountList implements ActionListener {
 						st.executeUpdate("insert into bank values (concat('" + acctype + gen + myString + "'),'"
 								+ nameText.getText() + "','" + addressText.getText() + "')");
 
-						f.add(new Label("Account number: "+acctype + gen + myString));
+						f.add(new Label("Account number: " + acctype + gen + myString));
 						f.setSize(150, 150);
 						f.setVisible(true);
 
@@ -151,4 +147,158 @@ class OpenAccountList implements ActionListener {
 		f.setVisible(true);
 
 	}
+}
+
+class Transactions extends KeyAdapter implements ActionListener{
+	JFrame transactions = new JFrame();
+	JLabel accLabel = new JLabel("Accno");
+	JTextField accNo = new JTextField(5);
+	JButton checkAcc = new JButton("...");
+	JLabel nameLabel = new JLabel("Name");
+	JLabel name = new JLabel();
+	JLabel addressLabel = new JLabel("Address");
+	JLabel address = new JLabel();
+	JLabel accountLabel = new JLabel("Account");
+	JLabel account = new JLabel();
+	String transac = "";
+	JLabel transacLabel = new JLabel(transac);
+	JTextField amount = new JTextField();
+	JLabel balanceLabel = new JLabel("Current Balance");
+	JLabel balance = new JLabel();
+	JLabel newbalanceLabel = new JLabel("New Balance");
+	JLabel newbalance = new JLabel();
+	JButton save = new JButton("Save");
+	int bal, val;
+
+	public void actionPerformed(ActionEvent e) {
+		bal = 0;
+		val = 0;
+		GroupLayout layout = new GroupLayout(transactions.getContentPane());
+		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+		GroupLayout.ParallelGroup h1Group = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+		GroupLayout.ParallelGroup h2Group = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+		GroupLayout.ParallelGroup h3Group = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+
+		transactions.getContentPane().setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+
+		layout.setVerticalGroup(vGroup);
+		layout.setHorizontalGroup(hGroup);
+		vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(accLabel)
+				.addComponent(accNo).addComponent(checkAcc));
+		hGroup.addGroup(h1Group).addGroup(h2Group).addGroup(h3Group);
+		h1Group.addComponent(accLabel);
+		h2Group.addComponent(accNo);
+		h3Group.addComponent(checkAcc);
+
+		transactions.setSize(400, 100);
+		transactions.setVisible(true);
+		checkAcc.addActionListener(this);
+		save.addActionListener(this);
+		amount.addKeyListener(this);
+		if (((Object) e.getSource()).equals(checkAcc) || ((Object) e.getSource()).equals(save)) {
+			try {
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost/banking?serverTimezone=BST",
+						"root", "");
+				Statement st = con.createStatement();
+				ResultSet rs = st
+						.executeQuery("select name, address from bank where accno = '" + accNo.getText() + "'");
+				if (rs.next()) {
+					h1Group.addComponent(nameLabel).addComponent(addressLabel).addComponent(accountLabel)
+							.addComponent(transacLabel).addComponent(newbalanceLabel);
+					h2Group.addComponent(name).addComponent(address).addComponent(account).addComponent(amount)
+							.addComponent(balanceLabel).addComponent(newbalance);
+					h3Group.addComponent(balance).addComponent(save);
+
+					vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(nameLabel)
+							.addComponent(name))
+							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+									.addComponent(addressLabel).addComponent(address))
+							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+									.addComponent(accountLabel).addComponent(account))
+							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+									.addComponent(transacLabel).addComponent(amount))
+							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+									.addComponent(balanceLabel).addComponent(balance))
+							.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+									.addComponent(newbalanceLabel).addComponent(newbalance).addComponent(save));
+
+					name.setText(rs.getString(1));
+					address.setText(rs.getString(2));
+					if (accNo.getText().substring(0, 1).equals("c")) {
+						account.setText("Current");
+					} else if (accNo.getText().substring(0, 1).equals("s")) {
+						account.setText("Saving");
+					} else
+						System.out.println("Error with account type comparison");
+					transacLabel.setText(transac);
+					rs = st.executeQuery("select sum(amount) from deposit where accno = '" + accNo.getText() + "'");
+					if (rs.next())
+						bal = rs.getInt(1);
+					else
+						bal = 0;
+					rs = st.executeQuery("select sum(amount) from withdraw where accno = '" + accNo.getText() + "'");
+					if (rs.next())
+						bal -= rs.getInt(1);
+					else
+						bal -= 0;
+					balance.setText(Integer.toString(bal));
+					transactions.setSize(400, 300);
+					if (((JButton) e.getSource()).equals(save) && !amount.getText().equals("")) {
+						if (transac.equals("Withdraw") || transac.equals("Deposit")) {
+							st.executeUpdate("insert into " + transac + " values ('" + accNo.getText() + "', " + val
+									+ ", current_date)");
+							amount.setText("");
+						}
+					}
+				} else
+					System.out.println("Account number not found");
+			} catch (Exception e2) {
+				System.out.println(e2.toString());
+				System.out.println("Something went wrong");
+			}
+		} else {
+			if (((JMenuItem) e.getSource()).getText().equals("Deposit Money")) {
+				transac = "Deposit";
+			} else if (((JMenuItem) e.getSource()).getText().equals("Withdraw Money")) {
+				transac = "Withdraw";
+			} else if (((JMenuItem) e.getSource()).getText().equals("Check Balance")) {
+				transac = "Check Balance";
+			} else
+				System.out.println("Error with String comparison");
+
+		}
+//		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+//		hGroup.addGroup(layout.createParallelGroup().addComponent(nameLabel).addComponent(addressLabel));
+//		hGroup.addGroup(layout.createParallelGroup().addComponent(nameText).addComponent(addressText)); //cant get grouplayout to work
+
+	}
+    public void keyReleased(KeyEvent e) {
+        JTextField textField = (JTextField) e.getSource();
+        try{
+        	if(textField.getText().equals(""))
+        		val = 0;
+        	else
+        		val = Integer.parseInt(textField.getText());
+			if (transac.equals("Withdraw")) {
+				if (bal - Integer.parseInt(textField.getText()) < 0)
+					System.out.println("Insufficient funds");
+				else
+					newbalance.setText(Integer.toString(bal - val));
+			} else
+				newbalance.setText(Integer.toString(bal + val));
+	    }
+        catch
+//        (NumberFormatException E) {
+//			System.out.println("Only numbers please");
+//			newbalance.setText("error");
+//        }
+        (Exception E) {
+			System.out.println(E.toString());
+			newbalance.setText("error");
+        }
+    }
+
 }
